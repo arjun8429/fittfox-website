0554d980f2b9deafe71265c04e60e73e0fceb267
arjun8429 <93005308+arjun8429@users.noreply.github.com>
2026-05-20 21:01:04 +0530

    Update EnquiryModal.jsx

--- src/components/EnquiryModal.jsx
+++ src/components/EnquiryModal.jsx
@@ -146,7 +146,7 @@ const SHARED_QUESTIONS = [
 const RATINGS = ["Poor", "Average", "Good", "Great", "Loved it!"];
 
 // Paste your Google Apps Script Web App URL here after deployment
-  const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwltEEKneNBH8Q0QXdrkKt0YrYSmZkHyIV62RGwXm0wdncR6_GkB72OJmw-1xzLMZywtw/exec";
+  const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwF7XPCSItdMVe9mbZVHq8X-7dTwR1t2Z1J-udNdgYOR9iM6IlrI075xiaJoKOm6dt-aw/exec";
   //const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyT8tT-a0yPiya9r97SC4tB691LN86cj6blHiN3lbiXmDti8HdQir24RaRReotWrXtD/exec"
 // ─── COMPONENT ─────────────────────────────────────────────────────────────────
 export default function SattuPoll({ open, onClose }) {
